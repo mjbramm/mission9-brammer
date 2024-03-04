241450e82f01e46315b9f9a34a20604ef5621a3c
mjbramm <mitchbrammer@gmail.com>
2024-03-03 19:13:35 -0700

    Working

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,69 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+interface TeamProps {
+  school: string;
+  name: string;
+  city: string;
+  state: string;
+}
+
+// This needs to be imported from the CollegeBasketballTeams.json
+const NCAAteams = [
+  {
+    school: 'School1',
+    name: 'name1',
+    city: 'City1',
+    state: 'state1',
+  },
+  {
+    school: 'School2',
+    name: 'name2',
+    city: 'City2',
+    state: 'state2',
+  },
+];
+
+class Team extends React.Component<TeamProps> {
+  render() {
+    const oneTeam = this.props;
+    return (
+      <div>
+        <h2>{oneTeam.school}</h2>
+        <h2>{oneTeam.name}</h2>
+        <h2>{oneTeam.city}</h2>
+        <h2>{oneTeam.state}</h2>
+      </div>
+    );
+  }
+}
+
+function TeamList() {
+  return (
+    <div>
+      <h1>List of teams in the team CollegeBasketballTeams.json file</h1>
+      <Team {...NCAAteams[0]} />
+      <Team {...NCAAteams[1]} />
+    </div>
+  );
+}
+
+function HeadingTop() {
+  return (
+    <header className="App-header">
+      <h1>
+        This website lists out information about all the colleges in NCAA
+        Basketball
+      </h1>
+    </header>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HeadingTop />
+      <TeamList />
     </div>
   );
 }
